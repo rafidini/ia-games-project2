c7fe54aa9586b9aed15acc0f758ee8d6d42fbb27
rafidini <itokiana.rafidinarivo@edu.esiee.fr>
2022-01-20 12:03:37 +0100

    debut mais absence de convergence

--- _AT_01 CartPole REINFORCE.py
+++ _AT_01 CartPole REINFORCE.py
@@ -27,12 +27,17 @@ Arret :
 import gym
 import time
 from pyglet.window import key
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 # Environnement du jeu
 env = gym.make('CartPole-v0')
 
-def politique():
-    """TODO
+# Modèle
+class Net(nn.Module):
+    """
     Réseau de neurones en forme standard pour décrire la politique (𝜋0) avec
     deux couches Linear de 20 neurones sur la couche intermédiaire.
 
@@ -47,15 +52,45 @@ def politique():
     car les poids des réseaux ont été initialisés au hasard. Si cette étape fonction-
     ne, la moitié du chemin a été effectué.
     """
-    pass
 
-def compute_loss():
-    #
-    """TODO
-    Calcul la loss.
-    """
-    pass
+    def __init__(self):
+        """
+        Constructeur naturel.
+        """
+        super(Net, self).__init__()
+        self.couche1 = nn.Linear(4, 20)
+        self.couche2 = nn.Linear(20, 2)
+        self.couche_output = nn.Softmax(dim=1)
+    
+    def forward(self, x):
+        """
+        Forward.
+        """
+        # Couche input
+        x_1 = self.couche1(x)
+        x_1 = F.relu(x_1)
 
+        # Couche intermediaire
+        x_2 = self.couche2(x_1)
+
+        # Output layer
+        output = self.couche_output(x_2)
+
+        return output
+
+    def Loss(self, R, LogProbas):
+        """
+        Fonction de cout.
+        """
+        loss_policy = list()
+        
+        for log_prob, reward in zip(LogProbas, R):
+            loss_policy.append(-log_prob * reward)
+
+        loss_policy = torch.cat(loss_policy).sum()
+
+        return loss_policy
+    
 def GetAction(x: int) -> int:
     """
     IA basique
@@ -71,62 +106,89 @@ def GetAction(x: int) -> int:
     """
     return 1 if x < 0 else 0
 
+def pi(probas_scores):
+    """
+    Tire aleatoirement la prochaine action par rapport aux probabilites
+    donnees.
+    """
+    m = torch.distributions.categorical.Categorical(probas_scores)
+
+    # Tirage aleatoire avec probas de l'action (0 ou 1)
+    index_action = m.sample()
+
+    # Calcul de la log proba
+    log_prob = m.log_prob(index_action)
+    return index_action.item(), log_prob
+
 # Parametres des simulations
-N_SIMULATIONS: int = 10
-LEARNING_RATE: float = 0.01
-#optimizer = optim.Adam(policy.parameters(), lr=LEARNING_RATE)
+RENDER: bool = False
+LOG_STEP: int = 20
+N_SIMULATIONS: int = int(1e3)
 results: dict = {
     'scores': []
 }
+
+# Parametres de politique
+LEARNING_RATE: float = 0.015
+policy = Net()
+optimizer = torch.optim.Adam(policy.parameters(), lr=LEARNING_RATE)
 
 # Main game loop
 for n in range(N_SIMULATIONS):
     # Gestion d'une simulation
     done = False
     state = env.reset()
-    TotalScore = 0
+    TotalReward = list()
+    LogProbas = list()
     env.reset()
 
-    while not done :
+    while not done:
         # Affiche la simulation
-        w = env.render()
+        if RENDER:
+          w = env.render()
 
         # Récupère la position du chariot
         x = state[0]
 
         # TODO Decide de l'action à effectuer
-        action = GetAction(x)
-        # action, log_prob = politique(x)
+        #action = GetAction(x)
+        tensor_state = torch.reshape(torch.FloatTensor(state), (1, -1))
+        action_probas = policy.forward(tensor_state)
+        action, log_prob = pi(action_probas)
         
         # Mise à jour de l'environnement
         state, reward, done, _ = env.step(action)
 
         # TODO Sauvegarde reward & log proba
-        TotalScore += reward
+        TotalReward.append(reward)
+        LogProbas.append(log_prob)
 
         # Frequence de rafraichissement de l'animation
-        time.sleep(0.01)
+        if RENDER:
+            time.sleep(0.01)
     
     # TODO Calcul de la loss
-    ...
+    LossTheta = policy.Loss(TotalReward, LogProbas)
 
     # TODO Descente du gradient
-    #optimizer.zero_grad()
-    #Loss.backward()
-    #optimizer.step()
+    optimizer.zero_grad()
+    LossTheta.backward()
+    optimizer.step()
 
     # Log du score
-    print(f"[{n+1}/{N_SIMULATIONS}] - Score final : {TotalScore}")
-    results['scores'].append(TotalScore)
+    if n % LOG_STEP == 0:
+        print(f"[{n+1}/{N_SIMULATIONS}] - Score({sum(TotalReward)}) - Loss({round(LossTheta.item(), 1)})")
+    results['scores'].append(sum(TotalReward))
 
 # Affichage des resultats
-print()
-print("-" * 30)
-print(f'Nombre de simulations : {N_SIMULATIONS}')
-print(f"Taux d'apprentissage  : {LEARNING_RATE}")
-print()
-print(f"Score moyen           : {sum(results['scores']) / len(results['scores'])}")
-print("-" * 30)
+SPACING: int = 50
+print("-" * SPACING)
+print(f'| Nombre de simulations : {N_SIMULATIONS}'.ljust(SPACING-1) + '|')
+print(f"| Learning rate         : {LEARNING_RATE}".ljust(SPACING-1) + '|')
+print("|".ljust(SPACING-1) + '|')
+print(f"| >> Resultats".ljust(SPACING-1) + '|')
+print(f"| Reward moyen          : {sum(results['scores']) / len(results['scores'])}".ljust(SPACING-1) + '|')
+print("-" * SPACING)
 
 env.close()
 
